34fbfe049e58e53746601bcd93fc82a33281b831
Jackshanaa <shanajack18@gmail.com>
2026-07-09 09:31:31 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/645-SetMismatch.java
@@ -0,0 +1,21 @@
+// Last updated: 09/07/2026, 09:31:30
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Set<Integer> set =new HashSet<>();
+        int[] ans = new int[2];
+        int n=nums.length;
+        for(int num:nums){
+            if(!set.add(num)){
+                ans[0]=num;
+            }
+        }
+        int setSum=0;
+        for(Integer num:set){
+            setSum+=num;
+        }
+        int sum=n*(n+1)/2;
+        ans[1]=sum-setSum;
+        return ans;
+    }
+}
+
