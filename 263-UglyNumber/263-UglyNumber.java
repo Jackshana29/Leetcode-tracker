d759cbeb2b929f1a872deba88d35bc3378ba759e
Jackshanaa <shanajack18@gmail.com>
2026-07-09 09:31:57 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 09/07/2026, 09:31:55
+class Solution {
+    public boolean isUgly(int n) {
+        if (n <= 0) return false;
+        int[] primes = {2, 3, 5};
+        for (int p : primes) {
+            while (n % p == 0) {
+                n /= p;
+            }
+        }
+        return n == 1;
+    }
+}
