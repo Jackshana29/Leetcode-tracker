e934f471b5ab51fd4ce3dc720294048f838621d2
Jackshanaa <shanajack18@gmail.com>
2026-07-09 09:30:38 +0530

    Create 2714-LeftAndRightSumDifferences

---
+++ 2714-LeftAndRightSumDifferences/2714-LeftAndRightSumDifferences.java
@@ -0,0 +1,23 @@
+// Last updated: 09/07/2026, 09:30:37
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int n = nums.length;
+        int leftSum[] = new int[n];
+        int rightSum[] = new int[n];
+        int result [] = new int[n];
+        
+        for(int i = 1; i < n;i++){
+            leftSum[i] = leftSum[i - 1] + nums[i - 1];
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            rightSum[i] = rightSum[i + 1] + nums[i + 1];
+        }
+
+        for(int i = 0; i < n; i++){
+            result[i] = Math.abs(leftSum[i] - rightSum[i]);
+        }
+
+        return result;
+    }
+}
