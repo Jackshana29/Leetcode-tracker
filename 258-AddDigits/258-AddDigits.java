001446fc8d259b94a823c0fd586e04f278122f89
Jackshanaa <shanajack18@gmail.com>
2026-07-09 09:31:58 +0530

    Create 258-AddDigits

---
+++ 258-AddDigits/258-AddDigits.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:31:56
+class Solution {
+    public int addDigits(int num) {
+        int sum=0;
+        if(num==0){
+            return 0;
+        }
+        else{
+            while(num>0){
+                int d=num%10;
+                sum=sum+d;
+                num=num/10;
+            }
+        }
+        if(sum>=10){
+            return addDigits(sum);
+             }
+         return sum;
+    }
+}
