cd0704259a035524b442aecff1973c365df6d985
Jackshanaa <shanajack18@gmail.com>
2026-07-09 09:30:53 +0530

    Create 1786-CountTheNumberOfConsistentStrings

---
+++ 1786-CountTheNumberOfConsistentStrings/1786-CountTheNumberOfConsistentStrings.java
@@ -0,0 +1,20 @@
+// Last updated: 09/07/2026, 09:30:52
+class Solution {
+    public int countConsistentStrings(String allowed, String[] words) {
+        int count = 0;
+        for (int i = 0; i < words.length; i++) {
+            String str = words[i];
+            boolean isValid = true;
+            for (int j = 0; j < str.length(); j++) {
+                char ch = str.charAt(j);
+                if (allowed.indexOf(ch) == -1) {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid)
+                count++;
+        }
+        return count;
+    }
+}
