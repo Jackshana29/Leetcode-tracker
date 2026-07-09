1db55843a423c14ba4060b61f3874971b96df784
Jackshanaa <shanajack18@gmail.com>
2026-07-09 09:30:20 +0530

    Create 3297-MinimumTimeToRevertWordToInitialStateI

---
+++ 3297-MinimumTimeToRevertWordToInitialStateI/3297-MinimumTimeToRevertWordToInitialStateI.java
@@ -0,0 +1,15 @@
+// Last updated: 09/07/2026, 09:30:19
+class Solution {
+    public int minimumTimeToInitialState(String word, int k) {
+        int count = 0;
+
+        for (int i = 1; i <= word.length() / k; i++) {
+            count = i;
+            if (word.substring(k * i).equals(word.substring(0, word.length() - k * i))) {
+                return i;
+            }
+        }
+
+        return count + 1;        
+    }
+}
